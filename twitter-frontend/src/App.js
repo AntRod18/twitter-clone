a7ba5fd96830fb612331d787440b267f7913b43a
AntRod18 <antrod1895@gmail.com>
2021-10-06 20:22:08 -0400

    Innitial commit

---
+++ twitter-frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Component} from "react"
+
+class App extends Component {
+  render(){
+  return (
+    <div className="app">
+      <h1> Yo Yo</h1>
+
+      {/* {Sidebar} */}
+
+      {/* {Feed} */}
+
+      {/* {Widgets} */}
+    </div>
+  );
+  }
+}
+
+export default App;
