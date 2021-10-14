3653b7f07b878b3580525f4261094d83748ff7e8
AntRod18 <antrod1895@gmail.com>
2021-10-14 14:51:46 -0400

    add tweet button

--- twitter-frontend/src/Sidebar.js
+++ twitter-frontend/src/Sidebar.js
@@ -10,6 +10,7 @@ import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from "@material-ui/core"
 
 function Sidebar() {
     return (
@@ -25,6 +26,8 @@ function Sidebar() {
             <SidebarOption Icon={ListAltIcon} text="Lists"/>
             <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
             <SidebarOption Icon={MoreHorizIcon} text="More"/>
+
+            <Button variant="outlined" className="sidebar_tweet" fullWidth >Tweet</Button>
             
         </div>
     )
