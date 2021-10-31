787581f37682d7711183d11a9d43add019f67139
AntRod18 <antrod1895@gmail.com>
2021-10-31 19:01:10 -0400

    style the sidebar

--- twitter-frontend/src/App.js
+++ twitter-frontend/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import React from "react"
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 class App extends React.Component {
   render(){
   return (
     <div className="app">
-      <h1> Yo Yo</h1>
-
       <Sidebar />
-
-      {/* {Feed} */}
-
+      <Feed />
       {/* {Widgets} */}
     </div>
   );
